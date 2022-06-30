f73f7f635e823d6e2728fc11f1c80c03a6b2d7fc
iankerich254 <iankerich254@gmail.com>
2022-06-30 14:02:38 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*Function main*/
-int main(void)/*Function to print text*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
