7ad3226c4c3b5056f794d5afb499876c4d76e590
iankerich254 <iankerich254@gmail.com>
2022-06-30 13:42:35 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,11 +11,11 @@
  */
 int main(void)
 {
-	/*
-	 * Function to print some text
-	 *
-	 */
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    /*
+     * Function to print some text
+     *
+     */
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
 
