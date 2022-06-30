0803f0fe0c9927bebd39477e2b457e5d14f88fc4
iankerich254 <iankerich254@gmail.com>
2022-06-30 13:58:06 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /*Function main*/
-int main(void)
-/*function to print text*/
+int main(void)/*Function to print text*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
