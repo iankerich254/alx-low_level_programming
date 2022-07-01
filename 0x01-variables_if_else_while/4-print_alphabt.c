fdf3da97b3452f81b6847c07adbd11562dbf5cad
iankerich254 <iankerich254@gmail.com>
2022-07-01 14:22:30 -0700

    Remove letters 'e' and 'q'

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,7 +15,7 @@ int main(void)
 	for (character = 'a'; character <= 'z'; character++)
 	{
 		if (character != 'e' && character != 'q')
-			putchar(character)
+			putchar(character);
 	}
 	putchar('\n');
 	return (0);
