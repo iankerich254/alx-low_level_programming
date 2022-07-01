9484381b0ed187668ed1eab60d1aabb1f126ca97
iankerich254 <iankerich254@gmail.com>
2022-07-01 09:06:03 -0700

    Print lowercase chatacters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* My program goes here */
+
+	for (character = 'a'; character <= 'z'; character++)
+		putchar(character);
+	putchar("\n");
+	return (0);
+}
