13979abf2a52ca64bc7aa67402a32bc0bc8fc93a
iankerich254 <iankerich254@gmail.com>
2022-07-05 12:02:03 -0700

    Print alphabets 10 times

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,18 +10,4 @@ int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 
-void print_alphabet_x10(void)
-{
-	char i;
-	int j;
-
-	for (j = 0; j < 10; j++)
-	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-	}
-	_putchar('\n');
-}
 #endif
