a697503e925dcd8ecc198f50816b503cd0c7bb66
iankerich254 <iankerich254@gmail.com>
2022-07-05 11:28:53 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	/* My code goes here */
 	char i;
@@ -14,5 +14,4 @@ int main(void)
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
