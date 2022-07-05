8c8a93f540ecee59b0398ff14d0e161aebbbeb75
iankerich254 <iankerich254@gmail.com>
2022-07-05 09:28:07 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* My code goes here */
+	printf("_putchar\n");
+	return 0;
+}
