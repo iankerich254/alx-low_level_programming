730d65e95b8d302493c12295d214af353e408a84
iankerich254 <iankerich254@gmail.com>
2022-07-05 12:17:35 -0700

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabets
+ */
+
+int _islower(int c)
+{
+	/* My code goes here */
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
