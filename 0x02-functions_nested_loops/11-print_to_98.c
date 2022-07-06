044aa9732240e8730be3d141cf441af99fa84576
iankerich254 <iankerich254@gmail.com>
2022-07-05 23:57:34 -0700

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,18 +1,29 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * print_to_98: prints integers starting from the number entered to 98
  *
- * @n - first integer in the parameter
+ * @n - the number to start iterating from
  *
  */
 
 void print_to_98(int n)
 {
 	/* My code goes here */
-	for (n = n; n <= 98; n++)
+	if (n >= 98)
 	{
-		printf("%d", n);
-		printf(", ");
+		while(n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while(n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
 	}
 }
