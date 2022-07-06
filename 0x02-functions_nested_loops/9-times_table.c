f84ec1d2baa9dbdd45edfbfd594825af36a0624a
iankerich254 <iankerich254@gmail.com>
2022-07-05 23:24:33 -0700

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,12 +11,22 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+
+		for (j = 1; j <= 9; j++)
 		{
-			prod = i * j;
-			_putchar(prod + '0');
 			_putchar(',');
-			_putchar('\t');
+			_putchar(' ');
+			
+			prod = i * j;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
+		_putchar('\n');
 	}
 }
