314a99c20e2b42b9e3964b16cfea2b556dcab492
iankerich254 <iankerich254@gmail.com>
2022-07-08 06:48:14 -0400

    Is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - function that checks for uppercase characters
+ * @c: an integer parameter
+ * Return: returns an int
+ */
+int _isupper(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
