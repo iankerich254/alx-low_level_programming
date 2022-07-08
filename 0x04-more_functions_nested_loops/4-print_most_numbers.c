ae28f6abe94e860ce6b71782dc338ae19327130a
iankerich254 <iankerich254@gmail.com>
2022-07-08 07:17:16 -0400

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,11 @@ void print_most_numbers(void)
 
 	for (j = 0; j < 10; j++)
 	{
-		if (j != 2 && j != 4)
+		_putchar(j + '0');
+
+		if (j == 2 || j == 4)
 		{
-			_putchar(j + '0');
+			continue;
 		}
 	}
 	_putchar('\n');
