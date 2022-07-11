57acaa061e18bd520f515df148b3f6e722eec13d
iankerich254 <iankerich254@gmail.com>
2022-07-11 21:19:56 +0300

    Keygen generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program to generate random passwords
+ * Return: 0
+ */
+
+int main(void)
+{
+	int pass[129];
+	int i, sum, n;
+
+	sum = 0;
+	srand(time(NULL));
+	for (i = 0; i < 129; i++)
+	{
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2500 - sum) - '0' < 78)
+		{
+			n = 2500 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
