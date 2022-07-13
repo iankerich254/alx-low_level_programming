b046a2b7c8636ff4a78edce2933edb673745e2a0
iankerich254 <iankerich254@gmail.com>
2022-07-13 22:36:32 +0300

    Capitalize strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper - converts strings to uppercase
+ * @s: first parameter
+  * Return: string capitalized
+ */
+char *string_toupper(char *s)
+{
+	int i;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] + 'A' - 'a';
+		}
+	}
+	return (s);
+}
