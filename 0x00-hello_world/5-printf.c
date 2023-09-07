7d11cb776e577f6f6dc8ef83cf4c04325d068bc2
Ian Kerich <iankerich254@gmail.com>
2023-09-07 12:56:54 -0700

    Print some text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ *
+ * Return: 0 which means it's successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
