953e35745b94d7045f0f3efd1ab356129c2aea4a
Ian Kerich <iankerich254@gmail.com>
2023-09-07 13:46:07 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 {
-	char *message = "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n";
+	char *message = "and that piece of art is useful\"- \
+			 Dora Korpar, 2015-10-19\n";
+
 	fwrite(message, 1, 59, stderr);
 	return (1);
 }
