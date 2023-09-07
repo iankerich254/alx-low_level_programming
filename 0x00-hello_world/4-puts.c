334c9e5c8a54855d062e38d4b5b01056c6c46221
Ian Kerich <iankerich254@gmail.com>
2023-09-07 12:54:00 -0700

    Print some text using puts, also add the repos README.md file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry into the program
+ *
+ * Return: 0 which means it's successful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+This is the Low Level Programming README.md file
