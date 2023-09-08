fd530f9868a31620eacd30d5061d032355ccaa5d
Ian Kerich <iankerich254@gmail.com>
2023-09-08 12:01:05 -0700

    Print all single digit numbers starting from 0 followed by a new line

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* The program goes here */
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
