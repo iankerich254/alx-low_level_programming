3df81101734e427093929b84020b72d0ef8261aa
Ian Kerich <iankerich254@gmail.com>
2023-09-12 16:42:21 -0700

    Compute the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function to compute absolute value of a number
+ *
+ * @n: number to be tested
+ *
+ * Return: absolute value returned
+ *
+ */
+int _abs(int n)
+{
+	/* The code goes here */
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+		return (-n);
+}
