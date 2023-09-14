2756dab5eb254279e011d752e101aa1eb7de9eaf
Ian Kerich <iankerich254@gmail.com>
2023-09-14 09:44:26 -0700

    Multiply tow integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiply two integers
+ *
+ * @a: first integer parameter
+ * @b: second integer parameter
+ *
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = a * b;
+	return (prod);
+}
