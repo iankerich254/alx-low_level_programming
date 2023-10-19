56ee81ec706cbc4ccc23410e76f14c9702c32997
iankerich254 <ankerich254@gmail.com>
2023-10-19 13:11:00 -0700

    Function that prints a certain message

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_message - function to print the message
+ * Return: none
+ */
+void print_message()
+{
+	printf("You're beat! and yet, you must allow,\\nI bore my house upon my back!\\n");
+}
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main()
+{	
+	/* Call the function to print the message */
+	print_message();
+	return 0;
+}
